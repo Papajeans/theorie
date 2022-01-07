ea95c4549a9348d1d4901bb77e32cac7723dbb05
JanV <jan.vercruysse@vdabcampus.be>
2022-01-07 17:53:04 +0100

    commit na testen tot Hfdst10"

--- src/main/java/be/vdab/theorie/domain/Rekening.java
+++ src/main/java/be/vdab/theorie/domain/Rekening.java
@@ -1,16 +1,28 @@
 package be.vdab.theorie.domain;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Rekening {
 
     private BigDecimal saldo = BigDecimal.ZERO;
+    private final List<BigDecimal> stortingen = new ArrayList<>();
     public void stort(BigDecimal bedrag) {
         if (bedrag.compareTo(BigDecimal.ZERO)<=0){
             throw new IllegalArgumentException("Het bedrag moet positief zijn");
         }
         saldo = saldo.add(bedrag);
+        stortingen.add(bedrag);
     }
     public BigDecimal getSaldo() {
-        return saldo;
+        return stortingen.stream()
+                .reduce(BigDecimal.ZERO,
+                        BigDecimal::add);
     }
+    public List<BigDecimal> getStortingen() {
+        return stortingen; }
+    public List<BigDecimal> getStortingenGesorteerd() {
+        return stortingen.stream().sorted().collect(Collectors.toList()); }
 }
--- src/test/java/be/vdab/theorie/domain/RekeningTest.java
+++ src/test/java/be/vdab/theorie/domain/RekeningTest.java
@@ -9,6 +9,35 @@ import static org.assertj.core.api.Assertions.assertThatNullPointerException;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 class RekeningTest {
+    private static final BigDecimal TWEE = BigDecimal.valueOf(2);
+    @Test
+    void eenNieuweRekeningHeeftGeenStortingen() {
+    var rekening = new Rekening();
+        assertThat(rekening.getStortingen()).isEmpty();
+    }
+    @Test
+    void nadatJe10EuroStortIsErEenStortingVan10Euro() {
+    var rekening = new Rekening();
+        rekening.stort(BigDecimal.TEN);
+        assertThat(rekening.getStortingen()).containsOnly(BigDecimal.TEN);
+    }
+    @Test
+    void nadatJe10EuroEn1EuroStortZijnDeStortingen10EuroEn1Euro() {
+    var rekening = new Rekening();
+        rekening.stort(BigDecimal.TEN);
+        rekening.stort(BigDecimal.ONE);
+        assertThat(rekening.getStortingen())
+                .containsExactly(BigDecimal.TEN, BigDecimal.ONE);
+    }
+    @Test
+    void nadatJe10EuroEn1EuroEn2EuroStortZijnDeStortingenGesorteerd1EuroEn2EuroEn10Euro() {
+    var rekening = new Rekening();
+        rekening.stort(BigDecimal.TEN);
+        rekening.stort(BigDecimal.ONE);
+        rekening.stort(TWEE);
+        assertThat(rekening.getStortingenGesorteerd()).isSorted()
+            .containsExactly(BigDecimal.ONE, TWEE, BigDecimal.TEN);
+    }
     @Test
     void hetSaldoVanEenNieuweRekeningIs0Euro() {
         var rekening = new Rekening();
